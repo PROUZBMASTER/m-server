7d264d9f0cc6becd14b91151e6af9aad9590d63e
Azizbek <PROUZBMASTER>
2023-05-26 19:04:37 +0500

    end

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonSever = require("json-server");
 const server = jsonSever.create();
 const router = jsonSever.router("db.json");
 const middlewares = jsonSever.defaults();
-const port = process.env.PORT || 1030;
+const port = process.env.PORT || 1010;
 
 server.use(middlewares);
 server.use(router);
